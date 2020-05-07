f71e9fdc59b2f7c73ee9a4828813811c7b798e97
LucasMCast <lucas.martins.c03@gmail.com>
2020-05-07 00:09:01 -0300

    first commit

---
+++  translate_country/country.py
@@ -0,0 +1,14 @@
+from en_us import COUNTRIES_ENUS
+from pt_br import COUNTRIES_PTBR
+
+def translate_en_to_pt(country_name):
+    """Função faz a tradução do nome do pais para portugues-br"""
+    country_pt = None
+
+    for key, value in COUNTRIES_ENUS.items():
+        country_name = country_name.lower()
+        value = value.lower()
+        if country_name == value:
+            country_pt = COUNTRIES_PTBR[key]
+
+    return country_pt
---
+++ README.md
@@ -0,0 +1,1 @@
+# Tranlate Name Country
